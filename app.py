271e938bf25137241e41239a1e645e1456747c48
Sujeet Kumar <sujeetkumarsk1501@gmail.com>
2026-01-21 04:09:49 +0530

    api changed

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import os
 # ===============================
 # 1. Gemini Setup
 # ===============================
-os.environ["GOOGLE_API_KEY"] = "AIzaSyBYOXgDZaX0NZQ7QENfm16QrXAthr72qeY"
+os.environ["GOOGLE_API_KEY"] = "AIzaSyDcXNKARy0zSGYwAsOB0HpX0AI6ViiteR4"
 genai.configure(api_key=os.environ["GOOGLE_API_KEY"])
 model = genai.GenerativeModel("gemini-flash-latest")
 
@@ -172,6 +172,7 @@ if content_to_process and st.button("✨ Simplify Agreement"):
         st.markdown(f"### 📝 Simplified Explanation ({target_language})")
         st.write(response.text)
         st.markdown("</div>", unsafe_allow_html=True)
+
 
 
 
